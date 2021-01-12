1ee4534bf96af20b2d4eef2183b55e6b3f7994d2
Kshitiz Ghimire <kshitiz.ghimire@anz.com>
2021-01-12 13:18:57 +1100

    add bottom stackview

--- ScrollStackView/ScrollStackView.swift
+++ ScrollStackView/ScrollStackView.swift
@@ -15,14 +15,33 @@ final class ScrollStackView: UIView {
         
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.layoutMargins = UIEdgeInsets(top: 0, left: 50, bottom: 0, right: 0)
         self.addSubview(scrollView)
+        
+        let bottomStackView = UIStackView(frame: .zero)
+        bottomStackView.axis = .vertical
+        bottomStackView.distribution = .fill
+        bottomStackView.addArrangedSubview(bottomLable)
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(bottomStackView)
         
         NSLayoutConstraint.activate([
             scrollView.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
             scrollView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor)
+            scrollView.bottomAnchor.constraint(equalTo: bottomStackView.topAnchor)
         ])
+        
+        let heightConstraintBottomStackView = bottomStackView.heightAnchor.constraint(greaterThanOrEqualToConstant: 0)
+              heightConstraintBottomStackView.priority = .defaultLow
+        NSLayoutConstraint.activate([
+            bottomStackView.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
+            bottomStackView.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
+            bottomStackView.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor),
+            heightConstraintBottomStackView
+            
+        ])
+        
         
         self.contentStackView.addArrangedSubview(self.lable)
         
@@ -69,4 +88,15 @@ final class ScrollStackView: UIView {
         label.text = "Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label Label"
         return label
     }()
+    
+    private lazy var bottomLable: UILabel = {
+           let label = UILabel(frame: .zero)
+           label.textAlignment = .center
+           label.adjustsFontForContentSizeCategory = true
+           label.numberOfLines = 0
+           label.font = UIFont.preferredFont(forTextStyle: .title1)
+           label.setContentCompressionResistancePriority(.required, for: .vertical)
+           label.text = "Bottom Label"
+           return label
+       }()
 }
